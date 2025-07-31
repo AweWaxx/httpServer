761c7ad493fb7d2e3b09e8e9e17d70b5c4f58d7c
Maxim <awewaxx@gmail.com>
2025-07-31 16:08:09 +0300

    Исправлены хендлеры и сервис

--- cmd/main.go
+++ cmd/main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"log"
+	"net"
 	"os"
+	"time"
 
 	"github.com/Yandex-Practicum/go1fl-sprint6-final/internal/server"
 )
@@ -10,9 +12,22 @@ import (
 func main() {
 	logger := log.New(os.Stdout, "SERVER: ", log.Ldate|log.Ltime|log.Lshortfile)
 
-	srv := server.New(logger)
+	if isPortInUse(":8080") {
+		logger.Fatal("Port 8080 is already in use")
+	}
 
+	srv := server.New(logger)
+	logger.Println("Starting server on :8080")
 	if err := srv.Start(); err != nil {
 		logger.Fatalf("Server failed: %v", err)
 	}
+}
+
+func isPortInUse(addr string) bool {
+	conn, err := net.DialTimeout("tcp", addr, 2*time.Second)
+	if err != nil {
+		return false
+	}
+	conn.Close()
+	return true
 }
